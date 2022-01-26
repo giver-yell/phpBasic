429ca3f04670a35f211cc83707e656c705ea499f
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-26 09:36:55 +0900

    初期画面修正 #1

---
+++ MyBBS/resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>My BBS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>My BBS</h1>
+    <ul>
+        <li>title</li>
+        <li>title</li>
+        <li>title</li>
+    </ul>
+</body>
+</html>
