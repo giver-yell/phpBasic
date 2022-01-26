a4da9d106d4efc2cd3fcb98aab8aff3400ef3892
Ryohei Fujihara <giver.yell@gmail.com>
2022-01-27 08:24:24 +0900

    controller #3

--- MyBBS/app/Http/Controllers/PostController.php
+++ MyBBS/app/Http/Controllers/PostController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = [
+            'title A',
+            'title B',
+            'title C'
+        ];
+
+        return view('index')
+            ->with(['posts' => $posts]);
+    }
 }
